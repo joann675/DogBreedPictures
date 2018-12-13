e730cbcbe20a00110ee42b0ed8b361133a6d23d8
joann675 <joann675@yahoo.com>
2018-12-12 19:34:07 -0500

    Adding rating tag and ability to get more images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
 var apiKey = "W8dSjJ0mp08muVgdx37ImPcoz1s8UzBa";
-var breedlist = ["Labrador Retreiver", "Bulldog", "German Shepherd", "Golden Retriever",
+var topics = ["Labrador Retreiver", "Bulldog", "German Shepherd", "Golden Retriever",
     "Beagle", "French Bulldog", "Poodle", "Yorkshire Terrier",
     "Rottweiler", "Boxer", "Dachshund"];
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=W8dSjJ0mp08muVgdx37ImPcoz1s8UzBa&limit=10&offset=0&rating=G&lang=en";
+var queryURL;
+var offset = 0;
 
 
 
@@ -15,14 +16,14 @@ $(document).ready(function () {
 
         // Logic modeled after working-movie-app exercise
         $("#DogBreedButtons").empty();
-        for (var i = 0; i < breedlist.length; i++) {
+        for (var i = 0; i < topics.length; i++) {
             var newButton = $("<button>");
             newButton.attr("style", "background:teal;color:white")
             newButton.addClass("breedButton");
             // Adding a data-attribute with a value of the breed at index i
-            newButton.attr("data-name", breedlist[i]);
+            newButton.attr("data-name", topics[i]);
             // Providing the button's text with a value of the breed at index i
-            newButton.text(breedlist[i]);
+            newButton.text(topics[i]);
             newButton.on("click", getImages);
             $("#DogBreedButtons").append(newButton);
 
@@ -33,9 +34,11 @@ $(document).ready(function () {
 
     function getImages() {
         console.log("In onClick for breed button")
+        offset = 0;
         $("#DogPictures").empty();
 
-        queryURL += "&q=" + $(this).attr("data-name");
+        queryURL = "https://api.giphy.com/v1/gifs/search?api_key=W8dSjJ0mp08muVgdx37ImPcoz1s8UzBa&limit=10&lang=en&q=" + $(this).attr("data-name") + "&offset=" + offset;
+        console.log(queryURL);
 
         $.ajax({
             url: queryURL,
@@ -43,26 +46,37 @@ $(document).ready(function () {
         })
             .then(function (response) {
                 console.log(response);
-                for (var j = 0; j < response.data.length; j++) {
-                    var imgStill = response.data[j].images.fixed_height_still.url;
-                    var imgMoving = response.data[j].images.fixed_height.url;
-                    var newImage = $("<img>");
-
-                    newImage.attr("src", imgStill);
-
-
-                    newImage.attr("data-still", imgStill);
-                    newImage.attr("data-animate", imgMoving);
-                    newImage.attr("data-state", "still");
-                    newImage.attr("style", "margin:10px");
-                    newImage.addClass("gif");
-                    newImage.on("click", changeState);
-                    $("#DogPictures").append(newImage);
-                }
+                renderImages(response);
 
             })
 
 
+    }
+
+    function renderImages(response) {
+        for (var j = 0; j < response.data.length; j++) {
+            var newDiv = $("<div>");
+            newDiv.attr("style", "display: inline-block");
+            var imgStill = response.data[j].images.fixed_height_still.url;
+            var imgMoving = response.data[j].images.fixed_height.url;
+            var rating = $("<h4>");
+
+            rating.text("Rating:" + response.data[j].rating);
+            var newImage = $("<img>");
+
+            newImage.attr("src", imgStill);
+
+
+            newImage.attr("data-still", imgStill);
+            newImage.attr("data-animate", imgMoving);
+            newImage.attr("data-state", "still");
+            newImage.attr("style", "margin:10px");
+            newImage.addClass("gif");
+            newImage.on("click", changeState);
+            newDiv.append(rating, newImage);
+            $("#DogPictures").prepend(newDiv);
+
+        }
     }
 
     // Logic taken from pausing-gifs exercise
@@ -89,16 +103,48 @@ $(document).ready(function () {
 
         // Get the text the user types into the input field
         var breed = $("#breed").val().trim();
-        // add the new breed into the breedlist array
-        breedlist.push(breed);
-        $("#breed").val("");
+        if (breed != "") {
+            // add the new breed into the breedlist array
+            topics.push(breed);
+            $("#breed").val("");
 
 
-        // The loadButtonList function is called, displaying the list of breed buttons
-        loadButtonList();
+            // The loadButtonList function is called, displaying the list of breed buttons
+            loadButtonList();
+        }
+    });
+
+    // Get the next 10 images
+    $("#getMore").on("click", function (event) {
+
+
+        if ($("#DogPictures").children().length == 0)
+            console.log("Can't get more if pictures are empty");
+        else {
+
+
+
+            event.preventDefault();
+            offset = parseInt(offset) + 10;
+            var n = queryURL.indexOf("&offset=");
+            queryURL = queryURL.slice(0, n) + "&offset=" + offset;
+            console.log(queryURL);
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+                .then(function (response) {
+                    console.log(response);
+                    renderImages(response);
+
+
+                });
+
+        }
+
     });
 
 
 
-});
-
+})
